7aa6d880ed3714093a532af40de82eff386bcf07
Emma Salot <emma.salot.etu@univ-lille.fr>
2025-05-07 10:00:27 +0200

    feat : add limiter for login and message request

--- route/message.route.js
+++ route/message.route.js
@@ -2,14 +2,15 @@ const express = require('express');
 const router = express.Router();
 const messageController = require('../controller/message.controller.js');
 const auth = require('../middleware/auth.middleware.js');
+const limiter = require("./../middleware/rateLimit.middleware.js");
 
-router.get('/', messageController.getAll);
-router.get('/:id', messageController.getById);
+router.get('/',() => limiter(10,100), messageController.getAll);
+router.get('/:id',() => limiter(10,100), messageController.getById);
 
-router.post('/',auth, messageController.create);
+router.post('/',auth,() => limiter(10,100), messageController.create);
 
-router.put('/:id',auth, messageController.update);
-router.delete('/:id',auth, messageController.remove);
+router.put('/:id',auth,() => limiter(10,100), messageController.update);
+router.delete('/:id',auth,() => limiter(10,100), messageController.remove);
 
 
 
